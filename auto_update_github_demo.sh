3fe2717db447a4660e5920d06cd9f86f571aaafc
JinChang1991 <zhangshengjin2012@163.com>
2020-12-13 18:00:35 +0800

    自动上传

--- auto_update_github_demo.sh
+++ auto_update_github_demo.sh
@@ -1,8 +1,5 @@
-git init
 git config --global user.name "JinChang1991"
 git config --global user.email "zhangshengjin2012@163.com"
-git branch -M master
-git remote add origin git@github.com:JinChang1991/demo.git
 git add .
 git commit -m "自动上传"
 git push -u origin master
